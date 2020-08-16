acd3d7045cdcef49f4b43d66b14f5a5e258ecc3a
gianmillare <gianmillaregsm@gmail.com>
2020-08-16 10:02:30 -0700

    added the floatingaction button dependency and added a button to add a new note into the app

--- Notable/app/src/main/java/io/gianmillare/notable/NotesDatabase.java
+++ Notable/app/src/main/java/io/gianmillare/notable/NotesDatabase.java
@@ -3,7 +3,7 @@ package io.gianmillare.notable;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {NoteDao.class}, version = 1)
+@Database(entities = {Note.class}, version = 1)
 public abstract class NotesDatabase extends RoomDatabase {
     public abstract NoteDao noteDao();
 }
