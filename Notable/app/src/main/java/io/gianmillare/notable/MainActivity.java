4819f6ca0db3c96c60e08dd7960f7af3fc77826b
gianmillare <gianmillaregsm@gmail.com>
2020-08-16 08:41:30 -0700

    configured the recyclerview with an ID, added recyclerview, NotableAdapter, and LayoutManager to the MainActivity file

--- Notable/app/src/main/java/io/gianmillare/notable/MainActivity.java
+++ Notable/app/src/main/java/io/gianmillare/notable/MainActivity.java
@@ -1,14 +1,26 @@
 package io.gianmillare.notable;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private NotableAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        layoutManager = new LinearLayoutManager(this);
+        adapter = new NotableAdapter();
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
     }
 }
